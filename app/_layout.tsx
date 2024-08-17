566c1c9af45a3a897fafbe2e5c6ae1dc8d984cbf
senethmendis <mendis.seneth@gmail.com>
2024-08-17 20:08:50 +0530

    "Added font loading and splash screen handling to RootLayout, implemented meditation timer and audio functionality in Meditate component, and updated package dependencies."

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
-import { Slot, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen, Stack } from "expo-router";
+import { preventAutoHideAsync } from "expo-splash-screen";
+import { useEffect } from "react";
+
+//prevet splash screen auto hiding
+//SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded, error] = useFonts({
+    "Roboto-Mono": require("../assets/fonts/RobotoMono-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) throw error;
+    if (fontsLoaded) SplashScreen.hideAsync();
+  }, []);
+
+  if (!fontsLoaded) return null;
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- app/meditate/[id].tsx
+++ app/meditate/[id].tsx
@@ -1,12 +1,50 @@
 import { View, Text, ImageBackground, Pressable } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MEDITATION_IMAGES from "@/constants/med-img";
 import AppGradient from "@/components/AppGradient";
 import { router, useLocalSearchParams } from "expo-router";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import CustomButton from "@/components/CustomButton";
+import { Audio } from "expo-av";
 
 const Meditate = () => {
   const { id } = useLocalSearchParams();
+  const [secondsRemaining, setSecondsRemaining] = useState(10);
+  const [isMeditating, setIsMeditating] = useState(false);
+  const [audioSound, setAudioSound] = useState<Audio.Sound>();
+  useEffect(() => {
+    let timerId: NodeJS.Timeout;
+
+    //exit
+    if (secondsRemaining === 0) {
+      setIsMeditating(false);
+      return;
+    }
+
+    if (isMeditating) {
+      timerId = setTimeout(() => {
+        setSecondsRemaining(secondsRemaining - 1);
+      }, 1000);
+    }
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [secondsRemaining, isMeditating]);
+
+  // format itme
+
+  const formattedTimeMinits = String(
+    Math.floor(secondsRemaining / 60)
+  ).padStart(2, "0");
+
+  const formtatterTimeSeconds = String(secondsRemaining % 60).padStart(2, "0");
+
+  const ToggleMeditationgSessionStatus = async () => {
+    if (secondsRemaining === 0) setSecondsRemaining(10);
+    setIsMeditating(!isMeditating);
+  };
+
   return (
     <View className="flex-1">
       <ImageBackground
@@ -21,6 +59,19 @@ const Meditate = () => {
           >
             <AntDesign name="leftcircleo" size={50} color="white" />
           </Pressable>
+          <View className="flex-1 justify-center">
+            <View className="mx-auto bg-neutral-200 rounded-full h-44 w-44 justify-center items-center">
+              <Text className="text-4xl text-blue-600 font-rmono">
+                {formattedTimeMinits}:{formtatterTimeSeconds}
+              </Text>
+            </View>
+          </View>
+          <View className="mb-5 ">
+            <CustomButton
+              title="Start Meditation"
+              onPress={() => setIsMeditating(true)}
+            />
+          </View>
         </AppGradient>
       </ImageBackground>
     </View>
